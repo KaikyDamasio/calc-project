d51391c70b7375824040a2e70639b540e2ecc895
Kaiky <kaikydamasio5@gmail.com>
2024-10-16 16:50:16 -0300

    code organization

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,19 @@
 </script>
 
 <template>
+  <body>
+    <header>
+      <h1 class="mb-4 mt-3 text-center">Calculadora Aritmética</h1>
+    </header>
     <div class="d-flex flex-column align-items-center">
-      <header>
-      <h1 class="mb-4 mt-3">Calculadora Aritmética</h1>
-      </header>
-      <div class="col-md-3 mb-3">
+    <div class="d-flex flex-column">
+      <div class="d-flex flex-row mb-3">
         <input class="text-center form-control" type="text" placeholder="Primeiro número">
       </div>
-      <div class="col-md-3 mb-3">
+      <div class="d-flex flex-row mb-3 gap-4">
         <input class="text-center form-control" type="text" placeholder="Segundo número">
       </div>
-      <div class="col-md-3 mb-3">
+      <div class="d-flex flex-row mb-3">
         <select class="form-control text-center" value="">
           <option value="" disabled selected>Selecione a operação</option>
           <option value="Multiplicação">Multiplicação</option>
@@ -23,8 +25,9 @@
         </select>
       </div>
     </div>
+  </div>
+  </body>
+
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
